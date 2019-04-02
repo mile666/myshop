c8c87b6be7fd7cdf27c7dda14ecbf9c41b09e1f1
mile <791144831@qq.com>
2019-04-02 21:50:43 +0800

    完成分类模块的搜索条效果

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>分类</div>
+  <div>
+    <!-- 搜索条 -->
+    <search-bar></search-bar>
+  </div>
 </template>
+<script>
+import SearchBar from '../../components/searchbar'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'search-bar': SearchBar
+  }
+}
+</script>
