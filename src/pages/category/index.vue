fb938eedd7c7d994e384412ccf2aeb05fe36d4a4
mile <791144831@qq.com>
2019-04-04 10:48:27 +0800

    完成分类数据的加载

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,18 +2,40 @@
   <div>
     <!-- 搜索条 -->
     <search-bar></search-bar>
+    <!-- 菜单和内容 -->
+    <div class="content">
+      <div class="left">
+        
+      </div>
+      <div class="right"></div>
+    </div>
   </div>
 </template>
 <script>
 import SearchBar from '../../components/searchbar'
+import request from '../../utils/request.js'
 export default {
   data () {
     return {
-
+      cate: []
     }
   },
   components: {
     'search-bar': SearchBar
+  },
+  methods: {
+    async cateData () {
+      // 调用接口获取分类数据
+      let ret = await request('categories')
+      this.cate = ret.data.message
+      // console.log(ret)
+    }
+  },
+  mounted () {
+    this.cateData()
   }
 }
 </script>
+<style scoped lang="scss">
+  @import "main.scss"
+</style>
