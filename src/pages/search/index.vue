b88ecf5288ea32b58220ecdd321578805df839a1
mile <791144831@qq.com>
2019-04-04 15:34:30 +0800

    完成关键字搜索功能数据加载和渲染

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -6,19 +6,38 @@
         <!-- 搜索输入框 -->
         <div class="search-input">
           <icon type='search' size='16'/> 
-          <input v-model='keyword' placeholder="请输入关键字"/>
+          <input @input='inputHandle' v-model='keyword' placeholder="请输入关键字"/>
         </div>
         <button v-if='keyword' class="cancel">取消</button>
+        <!-- 搜索结果 -->
+        <div class="search-modal">
+          <div :key='item.goods_id' v-for='item in searchResult' class="search-item">
+            {{item.goods_name}}
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import request from '../../utils/request.js'
 export default {
   data () {
     return {
-      keyword: ''
+      keyword: '',
+      searchResult: []
+    }
+  },
+  methods: {
+    async inputHandle () {
+      // 根据输入的关键字,调用后台接口查询匹配的数据
+      // request是一个异步的函数
+      let res = await request('goods/qsearch', 'get', {
+        query: this.keyword
+      })
+      const {message} = res.data
+      this.searchResult = message
     }
   }
 }
