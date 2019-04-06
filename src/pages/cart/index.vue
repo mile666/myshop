e3ac051fb2dda7611e3fbf71962271b22bbb3cf8
mile <791144831@qq.com>
2019-04-06 21:26:03 +0800

    完成购物车数据的获取操作

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -5,13 +5,39 @@
       <text>新增收货人</text>
       <span></span>
     </div>
+    <div class="list-title">优购生活馆</div>
+    <!-- 商品列表信息 -->
+    <div class="ware-list">
+      <div :key="item.goods_id" v-for="item in products" class="ware-item">
+        <div>{{item.goods_name}}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      products: []
     }
+  },
+  methods: {
+    getCartData () {
+      // 获取购物车数据
+      let cdata = mpvue.getStorageSync('mycart') || {}
+      // console.log(cdata)
+      // 把cdata对象转化为数组
+      let products = []
+      for (let key in cdata) {
+        // console.log(cdata[key])
+        products.push(cdata[key])
+      }
+      this.products = products
+    }
+  },
+  onLoad () {
+    // 从本地存储中获取购物车商品信息
+    this.getCartData()
   }
 }
 </script>
