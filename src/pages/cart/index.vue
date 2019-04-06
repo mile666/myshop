75312081e1de0a610478529fcb5b745488f83041
mile <791144831@qq.com>
2019-04-06 19:55:26 +0800

    完成购物车基本布局

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>购物车</div>
+  <div class="container">
+    <!-- 新增收货人信息 -->
+    <div class="add_addresss">
+      <text>新增收货人</text>
+      <span></span>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+@import 'main.scss'
+</style>
