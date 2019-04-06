5f04678957e32b9d2a30e75beaea75f9b87d92a6
mile <791144831@qq.com>
2019-04-06 12:31:53 +0800

    完成搜索列表数据加载更多操作

--- src/pages/search_list/index.vue
+++ src/pages/search_list/index.vue
@@ -46,6 +46,27 @@ export default {
     tabHandle (index) {
       // 修改当前tab的索引
       this.currentIndex = index
+    },
+    async loadData () {
+      // 通用的列表加载方法
+      // 根据关键字加载匹配的商品列表数据
+      // 调用后台接口获取数据
+      let res = await request('goods/search', 'get', {
+        // query: param.query
+        query: this.keyword,
+        pagenum: this.pagenum
+      })
+      // console.log(res)
+      let {message} = res.data
+      // 需要把新加载的一页数据添加到list中
+      let goods = [...this.list, ...message.goods]
+      this.list = goods
+      this.pagenum = parseInt(message.pagenum)
+      this.total = message.total
+      // 加载完成数据之后,让页码加1
+      // this.pagenum = this.pagenum + 1  //当成字符串处理了,需要转换
+      // this.pagenum = parseInt(this.pagenum) + 1
+      this.pagenum = this.pagenum + 1
     }
   },
   components: {
@@ -56,15 +77,14 @@ export default {
     // 参数query表示路径传递过来的参数
     // console.log(query)
     this.keyword = param.query
-    // 调用后台接口获取数据
-    let res = await request('goods/search', 'get', {
-      query: param.query
-    })
-    // console.log(res)
-    let {message} = res.data
-    this.list = message.goods
-    this.pagenum = message.pagenum
-    this.total = message.total
+    // 调用接口
+    // 页面初次展示的时候,调用loadData加载数据
+    this.loadData()
+  },
+  onReachBottom () {
+    // 滚动条触底的时候触发该方法
+    // console.log(111)
+    this.loadData()
   }
 }
 </script>
